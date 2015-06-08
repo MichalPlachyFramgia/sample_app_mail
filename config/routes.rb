4d64743559fd33d3078d6da2ee423501ac1323d3
michalplachyFramgia <michal.plachy@framgia.com>
2015-06-08 10:28:52 +0700

    Chapter.9

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   get    'login'   => 'sessions#new'
   post   'login'   => 'sessions#create'
   delete 'logout'  => 'sessions#destroy'
-
+  get 'demo' => 'static_pages#demo'
   resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
