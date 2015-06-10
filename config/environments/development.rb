95fcd2419840b0252e3fdab1530c2e374e625f81
michalplachyFramgia <michal.plachy@framgia.com>
2015-06-10 10:10:06 +0700

    SMTP

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,8 +16,15 @@ Rails.application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :test
-  config.action_mailer.default_url_options = { host: 'localhost:3000' }
-
+  config.action_mailer.default_url_options = { host: "localhost:3000" }
+  config.action_mailer.smtp_settings = {
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :user_name            => 'invisible.masks@gmail.com',
+    :password             => 'thresh256',
+    :authentication       => "plain",
+    :enable_starttls_auto => true
+  }
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
